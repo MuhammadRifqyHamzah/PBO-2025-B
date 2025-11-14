8af543ffc8fed12e1502d91091a1190896ba791d
MuhammadRifqyHamzah <muhammadrifqy051@gmail.com>
2025-11-14 16:35:36 +0700

    Update LuasLingkaran.php

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -1,33 +1,28 @@
 <?php
 
+namespace App\Math;
+
 class LuasLingkaran {
 
     public const phi = 3.14;
-
-    public int $jari;
-
-    public function __construct($isiJari = 1){
-        $this ->jari = $isiJari;
+    protected int $jari;
+    
+    public function __construct($isiJari = 1) {
+        $this->jari = $isiJari; //10
     }
 
-    public function tampil(){
-        $rumus = LuasLingkaran::phi * ($this->jari * $lingkaran->jari);
-        echo "Lingkaran {$nama} ini hasil nya adalah : {$rumus}";
+    public function tampil($nama = 'ban') {
+        $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
+        echo "Lingkaran {$nama} hasilnya adalah: {$rumus}";
     }
 
     public static function testing() {
         echo "<br/>";
-        echo "ini testing static";
+        echo "ini dari static";
     }
 
-    public function __destruct(){
+    public function __destruct() {
         echo "<br/>";
-        echo "Udahan";
+        echo "udah ah cape";
     }
 }
-
-$lingkaran = new LuasLingkaran();
-$lingkaran ->jari = 10;
-$lingkaran->tampil("roda");
-
-LuasLingkaran::tetsting();
