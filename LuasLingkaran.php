efeceab8cbf84004cb4c3ee3856fe48653be3f9e
MuhammadRifqyHamzah <muhammadrifqy051@gmail.com>
2025-11-14 14:39:45 +0700

    Method static

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -3,31 +3,21 @@
 class LuasLingkaran {
 
     public const phi = 3.14;
-
     public int $jari;
 
-    public function __construct($isiJari = 1){
-        $this ->jari = $isiJari;
-    }
-
-    public function tampil(){
-        $rumus = LuasLingkaran::phi * ($this->jari * $lingkaran->jari);
-        echo "Lingkaran {$nama} ini hasil nya adalah : {$rumus}";
+    public function tampil($nama = 'ban') {
+        $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
+        echo "Lingkaran {$nama} hasilnya adalah: {$rumus}";
     }
 
     public static function testing() {
         echo "<br/>";
-        echo "ini testing static";
-    }
-
-    public function __destruct(){
-        echo "<br/>";
-        echo "Udahan";
+        echo "ini dari static";
     }
 }
 
 $lingkaran = new LuasLingkaran();
-$lingkaran ->jari = 10;
-$lingkaran->tampil("roda");
+$lingkaran->jari = 12;
+$lingkaran->tampil('roda'); //panggil method tampil
 
-LuasLingkaran::tetsting();
+LuasLingkaran::testing(); //panggil static method
